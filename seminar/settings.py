e02614fd1a0d76639295b0e858f29f9012486c41
Hengky Liwandouw <hengkyliwandouw@gmail.com>
2019-05-05 01:23:19 +0800

    fixed space in jpegfile

--- seminar/settings.py
+++ seminar/settings.py
@@ -129,8 +129,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'seminar/static/')
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
