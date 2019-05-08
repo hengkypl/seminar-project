903a896946ac2a05f4840ef959e0b903cec1ad84
Hengky Liwandouw <hengkyliwandouw@gmail.com>
2019-05-08 20:49:49 +0800

    fixed peserta/urls

--- peserta/urls.py
+++ peserta/urls.py
@@ -5,4 +5,4 @@ from . import views
 urlpatterns = [
     path('', views.pesertahome, name='pesertahome'),
     path('register/', views.register, name='register'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
